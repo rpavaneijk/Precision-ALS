51d3ec3ad193e15ace4faa51afa39174aae792ad
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2023-03-04 15:19:20 +0100

    Add ext_main data file

---
+++ src/ext_common.R
@@ -0,0 +1,26 @@
+library(readxl)
+library(dplyr)
+library(stringr)
+
+ext_read <- function(path, ...) {
+    read_excel(path, na = c("Missing", "N/A", "NA", "Unknown"), ...)
+}
+
+ext_normalize_names <- function(xs) {
+    xs |>
+        rename_with(\(x) {
+            x |>
+                str_to_lower() |>
+                str_replace_all("<", "lt_") |>
+                str_replace_all(">", "gt_") |>
+                str_replace_all("[^A-Za-z0-9]+", "_") |>
+                str_replace_all("(^_)|(_$)", "")
+        })
+}
+
+ext_parse_boolean <- function(x) {
+    x |> case_match(
+        c("Yes", "yes") ~ TRUE,
+        c("No", "no") ~ FALSE
+    )
+}
---
+++ src/ext_main.R
@@ -0,0 +1,162 @@
+library(lubridate)
+
+source("src/ext_common.R")
+
+ext_main <- ext_read(
+    "data/P-ALS_Ext_Main_Data_File.xlsx",
+    col_types = c(
+        "text", # ID
+        "text", # Site
+        "text", # Sex
+        "date", # Date of Birth
+        "text", # Year/Year and Month of Birth
+        "numeric", # Age
+        "text", # First Symptom
+        "text", # Site of Onset
+        "skip", # Site of Onset 2
+        "skip", # Site of Onset 3
+        "skip", # Site of Onset 4
+        "text", # Side of Onset
+        "date", # Date of Onset
+        "date", # Month of Onset
+        "numeric", # Age at Onset
+        "numeric", # Calculated Age at Onset
+        "text", # Diagnosis
+        "text", # Diagnosis 2
+        "text", # Diagnosis 3
+        "text", # Other Diagnosis
+        "text", # Motor Neuron Predominance
+        "date", # Date of Diagnosis
+        "date", # Month of Diagnosis
+        "numeric", # Age at Diagnosis
+        "numeric", # Calculated Age at Diagnosis
+        "text", # Vital Status
+        "date", # Date of Death
+        "numeric", # Age at Death
+        "numeric", # Calculated Age at Death
+        "text", # Tracheostomy
+        "date", # Date of Tracheostomy
+        "numeric", # Age at Tracheostomy
+        "text", # >23h NIV
+        "date", # Date of 23h NIV
+        "numeric", # Age at >23 h NIV
+        "date", # Date of Last Follow Up
+        "numeric", # Age at Last Follow-up (if alive)
+        "date", # Date of Transfer
+        "numeric", # Age at Transfer (if alive)
+        "text", # Non-invasive Ventilation
+        "date", # Date of Non-invasive Ventialtion
+        "numeric", # Age at Non-invasive Ventilation
+        "text", # Gastrostomy
+        "date", # Date of Gastrostomy
+        "numeric", # Age at Gastrostomy
+        "text", # C9orf72 Tested
+        "text", # C9orf72 Status
+        "text", # Commercial Result
+        "text", # SOD1 Tested
+        "text", # SOD1 Status
+        "text", # FUS Tested
+        "text", # FUS Status
+        "text", # TARDBP Tested
+        "text", # TARDBP Status
+        "text", # Riluzole Use
+        "date", # Riluzole Start Date
+        "numeric", # Riluzole Start Age
+        "text", # Rilzole Stopped
+        "date", # Riluzole Stop Date
+        "text", # Edaravone Use
+        "text", # Edaravone Stopped
+        "date", # Edaravone Stop Date
+        "text" # Current Working Status
+    )
+) |>
+    ext_normalize_names() |>
+    rename_with(\(x) str_replace(x, "non_invasive_venti(la|al)tion$", "niv")) |>
+    rename_with(\(x) str_replace(x, "rilzole", "riluzole")) |>
+    mutate(
+        across(ends_with("_tested"), ext_parse_boolean),
+        sex = case_match(
+            sex,
+            c("Man", "Male") ~ "M",
+            c("Woman", "Female") ~ "F",
+        ),
+        year_of_birth = coalesce(
+            year(date_of_birth),
+            as.integer(str_extract(year_year_and_month_of_birth, "\\d{4}")),
+            as.integer(str_extract(year_year_and_month_of_birth, "(\\d{4})-\\d{1,2}", group = 1))
+        ),
+        bulbar_onset = site_of_onset %in% c(
+            "Bulbar", "Bulbaire", "Bulbar and Spinal",
+            "Bulbar and Spinal",
+            "Bulbar and Cognitive/Behavioural",
+            "Bulbar and Thoracic/Respiratory",
+            "Cognitive/Behavioural and Bulbar",
+            "PBP"
+        ),
+        cervical_onset = site_of_onset %in% c(
+            "Cervical", "Neck"
+        ),
+        spinal_onset = site_of_onset %in% c(
+            "Arms", "Spinal", "Bulbar and Spinal",
+            "Spinal and Cognitive/Behavioural",
+            "Cognitive/Behavioural and Spinal",
+            "Thoracic/Respiratory and Spinal",
+            "Membre supérieur distal Bilat",
+            "Membre inférieur distal D",
+            "Membre supérieur distal G",
+            "Membre inférieur proximal D",
+            "Membre inférieur proximal Bilat",
+            "Membre inférieur distal Bilat",
+            "Membre inférieur distal G",
+            "Membre supérieur distal D",
+            "Membre inférieur proximal G",
+            "Membre supérieur proximal G",
+            "Membre supérieur proximal Bilat",
+            "Membre supérieur proximal D",
+            "Upper limb", "Lower limb",
+            "Flail-Leg", "Flail-Arm",
+            "Hemiplegic"
+        ),
+        respiratory_onset = site_of_onset %in% c(
+            "Bulbar and Thoracic/Respiratory",
+            "Respiratory",
+            "Respiratoire",
+            "Thoracic/respiratory",
+            "Thoracic/Respiratory",
+            "Thoracic/Respiratory and Spinal"
+        ),
+        cognitive_onset = site_of_onset %in% c(
+            "Cognitive",
+            "Cognitive/Behavioural",
+            "Cognitive/Behavioural and Bulbar",
+            "Cognitive/Behavioural and Spinal",
+            "Cognitive impairment",
+            "FTD"
+        ),
+        proximal_onset = site_of_onset %in% c(
+            "Flail-Arm", "Flail-Leg",
+            "Membre supérieur proximal D",
+            "Membre supérieur proximal G",
+            "Membre supérieur proximal Bilat",
+            "Membre inférieur proximal D",
+            "Membre inférieur proximal G",
+            "Membre inférieur proximal Bilat",
+            "Neck", "Trunk", "trunk"
+        ),
+        distal_onset = site_of_onset %in% c(
+            "Membre supérieur distal D",
+            "Membre supérieur distal G",
+            "Membre supérieur distal Bilat",
+            "Membre inférieur distal D",
+            "Membre inférieur distal G",
+            "Membre inférieur distal Bilat"
+        ),
+        side_of_onset = case_when(
+            side_of_onset == "Right" ~ "R",
+            side_of_onset == "Left" ~ "L",
+            side_of_onset == "Both sides" ~ "B",
+            str_ends(site_of_onset, " D") ~ "R",
+            str_ends(site_of_onset, " G") ~ "L",
+            str_ends(site_of_onset, " Bilat") ~ "B"
+        ),
+    )
