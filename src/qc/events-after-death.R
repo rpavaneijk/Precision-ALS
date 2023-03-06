009549c06dfca95b463ec92d68deaa02ad5ae9aa
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2023-03-06 23:45:02 +0100

    Implement events-after-death qc script

---
+++ src/qc/events-after-death.R
@@ -0,0 +1,40 @@
+library(writexl)
+
+source("src/ext_main.R")
+
+gastrostomy_after_death <- ext_main |>
+    select(
+        site, id,
+        date_of_gastrostomy, age_at_gastrostomy,
+        date_of_death, age_at_death
+    ) |>
+    filter(
+        age_at_gastrostomy > age_at_death | date_of_gastrostomy > date_of_death
+    )
+
+tracheostomy_after_death <- ext_main |>
+    select(
+        site, id,
+        date_of_tracheostomy, age_at_tracheostomy,
+        date_of_death, age_at_death
+    ) |>
+    filter(
+        age_at_tracheostomy > age_at_death | date_of_tracheostomy > date_of_death
+    )
+
+niv_after_death <- ext_main |>
+    select(
+        site, id,
+        date_of_niv, age_at_niv,
+        date_of_death, age_at_death
+    ) |>
+    filter(
+        age_at_niv > age_at_death | date_of_niv > date_of_death
+    )
+
+dir.create("output/qc", showWarnings = FALSE)
+write_xlsx(list(
+    "Gastrostomy" = gastrostomy_after_death,
+    "NIV" = niv_after_death,
+    "Tracheostomy" = tracheostomy_after_death
+), "output/qc/events-after-death.xlsx")
