604b2ed87b6f4fad3968e93d7c45c9b8f941c30e
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2023-03-06 23:52:15 +0100

    Implement events-before-diagnosis qc script

---
+++ src/qc/events-before-onset.R
@@ -0,0 +1,40 @@
+library(writexl)
+
+source("src/ext_main.R")
+
+gastrostomy_before_onset <- ext_main |>
+    select(
+        site, id,
+        date_of_gastrostomy, age_at_gastrostomy,
+        date_of_onset, age_at_onset
+    ) |>
+    filter(
+        age_at_gastrostomy < age_at_onset | date_of_gastrostomy < date_of_onset
+    )
+
+tracheostomy_before_onset <- ext_main |>
+    select(
+        site, id,
+        date_of_tracheostomy, age_at_tracheostomy,
+        date_of_onset, age_at_onset
+    ) |>
+    filter(
+        age_at_tracheostomy < age_at_onset | date_of_tracheostomy < date_of_onset
+    )
+
+niv_before_onset <- ext_main |>
+    select(
+        site, id,
+        date_of_niv, age_at_niv,
+        date_of_onset, age_at_onset
+    ) |>
+    filter(
+        age_at_niv < age_at_onset | date_of_niv < date_of_onset
+    )
+
+dir.create("output/qc", showWarnings = FALSE)
+write_xlsx(list(
+    "Gastrostomy" = gastrostomy_before_onset,
+    "NIV" = niv_before_onset,
+    "Tracheostomy" = tracheostomy_before_onset
+), "output/qc/events-before-onset.xlsx")
