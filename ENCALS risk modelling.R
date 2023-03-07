c81e88c7abbfcda90bd164e85377d8344c9ef1e3
opiemartin <sarah.martin@kcl.ac.uk>
2023-03-07 17:50:23 +0000

    changes to the ENCALS risk modelling file - added my own function based on the published version of the model but I have some questions on the version you are using without C9 status? These are written as comments starting ##### SOM comment:

--- ENCALS risk modelling.R
+++ ENCALS risk modelling.R
@@ -9,6 +9,9 @@
 # ftd = presence of ALS-FTD at screening (T/F)
 # ee = definite el escorial at screening (T/F)
 
+#### SOM comment - just wondering what you do for C9 status? The TRICALS beta profile seems to have slightly different betas
+#### than the one I use, is this a version of the model without the C9 status?
+
 ## Create data from input parameters:
 D <- data.frame (AGE_ONSET = as.numeric (as.Date (onset) - as.Date (birth))/365.25,
                  DISDUR = (as.numeric (as.Date (date) - as.Date (onset))/365.25)*12,
@@ -34,3 +37,110 @@ D$LP <- (-1.839*D$tSLOPE) + (-2.376*D$tDXDELAY) + (-0.264*D$tAGE) + (0.474*D$tFV
 
 ## Eligibility (e.g. range -6.00 [lb] to -2.00[ub]):
 if (D$LP >= lb & D$LP <= ub){"ELIGIBLE"}else{"NOT ELIGIBLE"}
+
+##### SOM comment: Some of this code duplicates yours but it extends it with a function to predict survival time
+##### I don't mind how we put it together but just adding for now
+##### If we want to go down this route I also have code to impute some variables (eg C9 status) and then combine
+##### survival probabilities using multiple imputation rules (Rubin's rules)
+
+# FUNCTION SURVIVAL TIME BASED ON ENCALS MODEL
+
+encals_beta_function <- function(age, als_frs, diag_delay, el_escorial, onset, ftd, fvc, c9orf72) {
+  
+  
+  ### model covariates
+  b1_coef <- -1.837
+  b2_coef <- -2.373
+  b3_coef <- -0.267
+  b4_coef <-  0.477
+  b5_coef <-  0.269
+  b6_coef <-  0.233
+  b7_coef <-  0.388
+  b8_coef <-  0.256
+  
+  ### transformations
+  ## ALSFRSR SLOPE - if als-frs isn't already rate of decline
+  #alsfrsr_slope <- (48-als_frs)/diag_delay
+  
+  b1_trans <- (als_frs+0.1)^-0.5
+  b2_trans <- ((diag_delay/10)^-0.5)+log(diag_delay/10)
+  b3_trans <- (age/100)^-2
+  b4_trans <- ((fvc/100)^-1)+((fvc/100)^-0.5)
+  
+  ##### get total value of beta
+  b1 <- b1_coef*b1_trans
+  b2 <- b2_coef*b2_trans
+  b3 <- b3_coef*b3_trans
+  b4 <- b4_coef*b4_trans
+  b5 <- b5_coef*onset
+  b6 <- b6_coef*el_escorial
+  b7 <- b7_coef*ftd
+  b8 <- b8_coef*c9orf72
+  
+  total_b = b1+b2+b3+b4+b5+b6+b7+b8
+  
+  return (total_b)
+  
+}
+
+
+encals_survival_function <- function(encals_beta){
+  
+  if(is.na(encals_beta)){
+    encals_survival =NA
+  }  else {
+    ### spline model
+    ## spline coefficients
+    y0 <- -6.409
+    y1 <-  2.643
+    y2 <- -0.546
+    y3 <-  0.585
+    
+    ## knot positions
+    kmin <- 0.081
+    k1   <- 3.108
+    k2   <- 3.638
+    kmax <- 5.917
+    
+    ## lambdas
+    lambda_1 <- 0.481323
+    lambda_2 <- 0.390507
+    
+    time <- 1
+    survival_probability <- 1
+    survival_probabilities <- data.frame(time=numeric(),surv_prob=numeric())
+    
+    
+    while (survival_probability > 0.2) {
+      
+      log_time <- log(time)
+      y1z1 <- y1*log_time
+      y2z2 <- y2*((max((log_time-k1)^3,0))-lambda_1*(max((log_time-kmin)^3,0))-(1-lambda_1)*(max((log_time-kmax),0)))
+      y3z3 <- y3*(max((log_time-k2)^3,0)-lambda_2*max((log_time-kmin)^3,0)-(1-lambda_2)*max((log_time-kmax),0))
+      sx <- y0 + y1z1 + y2z2 + y3z3
+      
+      cumulative_log_odds <- exp(sx + encals_beta)
+      
+      survival_probability <- (1+cumulative_log_odds)^-1
+      
+      temp<-data.frame(time=numeric(),surv_prob=numeric())
+      temp[1,1]<-time
+      temp[1,2]<-survival_probability
+      
+      survival_probabilities<-rbind(temp,survival_probabilities)
+      
+      time <-  time+1
+    } ## end while loop
+    
+    ### pick closest to randomly generated survival probability
+    ### can set generated prob as median if preferable
+    generated_prob <- rtruncnorm(n=1, a=0, b=1, mean=0.5, sd=0.1)
+    median_survival <- survival_probabilities %>% 
+      arrange(abs(surv_prob - generated_prob)) %>%
+      slice(1)
+    
+    encals_survival <- median_survival$time
+    return(encals_survival)
+  }## end if encals survival is na loop
+  
+}
