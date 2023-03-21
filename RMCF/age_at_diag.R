6c445ff2cdee7c177b8697c181bc16088c341dca
rmcf4 <mcfarlro@tcd.ie>
2023-03-21 15:07:30 +0000

    small changes

--- RMCF/age_at_diag.R
+++ RMCF/age_at_diag.R
@@ -1,10 +1,17 @@
 library('tidyverse')
-#Is the age at ddiagnosis different at each site?
+#Is the age at diagnosis different at each site?
 ext_main %>% 
   filter(site != 'Karolinsk') %>% #Removing for now to investigate why
   ggplot(aes(x = calculated_age_at_diagnosis, fill = site)) +
   geom_histogram() +
   facet_wrap(~site)
+
+#Karolinsk is missing this - checked with Éanna, should be removed for age of diag anal
+
+ext_main %>% 
+  filter(site == 'Karolinsk')
+  select(ext_main$calculated_age_at_diagnosis)
+unique(ext_main$site)
 
 #I almost prefer it without the facet wrap actually, clearer image that not that different
 ext_main %>% 
@@ -24,4 +31,10 @@ ext_main %>%
   filter(site != 'Karolinsk') %>% 
   ggplot(aes(x= site, y= calculated_age_at_diagnosis, fill = site)) + 
   geom_violin()
- 
+
+#Age of onset may well be good to collect too
+ext_main %>% 
+  filter(date_of_diagnosis)
+  ggplot(aes(x = site, y = calculated_age_at_onset, fill = site)) +
+  geom_violin()
+  
